08d8c30b663437596d556b692655ccf732eb613d
Nina Saunders <nsaunders@smith.edu>
2024-10-08 11:57:01 -0400

    I applied the feedback I was given on my code.

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,83 @@
-class Conversation {
+import java.util.Random; 
+import java.util.Scanner;
+import java.util.ArrayList;
 
-  public static void main(String[] arguments) {
-    // You will start the conversation here.
-  }
-}
+public class Conversation {
+    // attributes 
+    // Number of rounds needed 
+    public Integer rounds;  
+    
+    public static void main(String[] arguments) {
+        // input Scanner 
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many rounds do you want? "); 
+        int rounds = input.nextInt();
+        input.nextLine();
+        
+        // Transcript Array List 
+        ArrayList < String > transcript = new ArrayList<>();
+        
+        //Random Responses 
+        String [] responses = {"Ok!", "Really!", "No Way!"};
+        Random random = new Random (); 
+
+        //Greeting 
+        String greeting = "Hello! What's on your mind?";
+        System.out.println(greeting);
+
+        // makes bot work 
+        for(int i=0; i< rounds; i++){
+            String answer = input.nextLine();
+            String [] splitAnswer = answer.split(" "); 
+            transcript.add(answer); 
+
+        // filters through the split responses and mirrors the words  
+            for(int j=0; j< splitAnswer.length; j++){
+                if (splitAnswer[j].equals ("I")){
+                    splitAnswer[j] = "You"; 
+                }
+                if (splitAnswer[j].equals ("me")){
+                    splitAnswer[j] = "you";
+                }
+                if (splitAnswer[j].equals ("am")){
+                    splitAnswer[j] = "are";
+                }
+                if (splitAnswer[j].equals ("you")){
+                    splitAnswer[j] = "I";
+                }
+                if (splitAnswer[j].equals ("my")){
+                    splitAnswer[j] = "your";
+                }
+                if (splitAnswer[j].equals ("your")){
+                    splitAnswer[j] = "my";
+                }
+            }
+            // puts split sentence back into a sentence 
+            String mirrorAnswer = "";
+            for(int k=0; k< splitAnswer.length; k++){
+                mirrorAnswer = mirrorAnswer + splitAnswer[k] + " ";
+            }
+
+            // provides automated responses 
+            if (mirrorAnswer.contains(answer)){
+                int randomIndex = random.nextInt(responses.length);
+                String randomResponse = responses [randomIndex]; 
+                System.out.println(randomResponse);
+                transcript.add(randomResponse);
+            }else{
+                System.out.println(mirrorAnswer);
+                transcript.add(mirrorAnswer);
+            }
+            
+        }
+            System.out.println("Ok! Goodbye");
+            System.out.println();
+            System.out.println("Transcript:");
+            for (int i=0; i < transcript.size (); i++){
+                System.out.println(transcript.get(i));
+            }
+            
+        
+        }
+    }
+
--- reflection.md
+++ reflection.md
@@ -1,7 +1,15 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+   I worked/consulted with Sofia, Liyana and Miriam. 
 - Which session(s) of TA / office hours did you attend?
+    I attended Ab's office hours on 9/19 and TA hours on 9/24. 
 - What are your initial impressions of Java? 
+I like Java a lot more than python. It is a little harder to work with and get oriented, however, not having to use white space is a lot easier for me. I also like how it is all object oriented over having the choice which for this course makes it easier to use.  
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+From looking at the two languages I can see some similarities between the two languages. They both rely on a sequential organization and have a set procedure. They also both start with declaring variables and transporting informaition and then have some main function that makes evreything work. 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+This assignment was difficult for me. I would highly recomond for someone else taking this course to write out all of the tasks and then orgnaize your code before you write it. I also would highly recomond using levels in the rubric as they helped to guide me a lot. Lastly, I would make sure to utalize all your resources including TA hours and your peers as they can be a really good resource. 
+
+Citations: 
+I looked online on how to create my randomizing function. 
